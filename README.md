4a552edb3fed57781cd913117cf1a42e24335c2b
Impre-visible <69050895+Impre-visible@users.noreply.github.com>
2022-03-09 19:46:26 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -21,25 +21,28 @@ Two way :
     ![Usage Button](images/usageButton.gif)
 
 
-## Known Issues
-
- - You have to press two times the VSCopilot button, or the command
-
 ## Release Notes
 
- - Only 150 requests every day
- - Nom you can use button in StatusBar to do the search
- - Only for comments
+ - Increase numbers of requests
 
 ## To Do
 
  - Add function analyze
- - Increase number of requests
+
+### 0.0.2
+
+ - Added more requests (now 5k)
 
 ### 0.0.1
 
-Work with JS, TS, Go and Python
+ - Work with JS, TS, Go and Python
+ - Nom you can use button in StatusBar to do the search
+ - Only for comments
 
+
+## Remerciements
+
+Merci à Trax et Symbiosis pour leur aide. Ce projet n'existerait pas sans eux !
 
 -----------------------------------------------------------------------------------------------------------
 
--- extension.js
+++ extension.js
@@ -2,6 +2,85 @@ const vscode = require('vscode');
 const axios = require('axios');
 const jsdom = require("jsdom");
 const https = require('https');
+const express = require('express');
+const passport = require('passport');
+const StackExchangeStrategy = require('passport-stack-exchange').Strategy;
+const open = require('open');
+const STORAGE_KEY = 'stackexchange_token';
+
+let token;
+let extensionContext;
+let server;
+
+/**
+ * Configurations.
+ */
+
+let STACK_EXCHANGE_APP_ID = '23031', // '*** YOUR APP ID (CLIENT ID) ***',
+    STACK_EXCHANGE_APP_SECRET = 'yabxjcApKEczmsSXoYQG8g((', //'*** YOUR APP SECRET (CLIENT SECRET) ***',
+    STACK_EXCHANGE_APP_KEY = 'MAsTBc4ad47BAQJOnCiZWQ((';
+
+function authentication() {
+    return new Promise(async(resolve, reject) => {
+        let timeout = setTimeout(() => {
+            reject();
+            server.close();
+        }, 60000);
+
+        /*
+         Passport session setup
+         */
+        passport.serializeUser(function(user, done) {
+            done(null, user);
+        });
+
+        passport.deserializeUser(function(obj, done) {
+            done(null, obj);
+        });
+
+        /*
+         Use the StackExchangeStrategy within Passport
+         */
+        passport.use(new StackExchangeStrategy({
+                clientID: STACK_EXCHANGE_APP_ID,
+                clientSecret: STACK_EXCHANGE_APP_SECRET,
+                callbackURL: 'http://127.0.0.1:3000/auth/stack-exchange/callback',
+                stackAppsKey: STACK_EXCHANGE_APP_KEY,
+                scope: 'no_expiry',
+                site: 'stackoverflow'
+            },
+            function(accessToken, refreshToken, profile, done) {
+                console.log("Token :", accessToken);
+                resolve(accessToken);
+                clearTimeout(timeout);
+
+                process.nextTick(function() {
+                    server.close();
+                    return done(null, profile);
+                });
+            }
+        ));
+
+        /**
+         * Configure Express
+         */
+        let app = express();
+        app.use(passport.initialize());
+
+        app.get('/auth/stack-exchange', passport.authenticate('stack-exchange'));
+
+        app.get('/auth/stack-exchange/callback', passport.authenticate('stack-exchange', { failureRedirect: '/auth/stack-exchange' }),
+            function(req, res) {
+                res.send("You can close this tab.");
+            });
+
+        server = app.listen(3000);
+
+        open('http://127.0.0.1:3000/auth/stack-exchange');
+
+
+    });
+}
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 const agent = new https.Agent({
@@ -21,6 +100,11 @@ const languages = { "js": "javascript", "py": "python", "pyw": "python", "ts": "
 function activate(context) {
 
     console.log('VSCopilot est lancé!');
+    extensionContext = context;
+
+    //Récupération du token
+    token = context.workspaceState.get(STORAGE_KEY, undefined);
+
     let disposable = vscode.commands.registerCommand('vscopilot.openCopilot', async() => {
         // Verify if user is in a project
         const editor = vscode.window.activeTextEditor;
@@ -89,25 +173,30 @@ function activate(context) {
 function deactivate() {}
 
 async function openCopilot(language, sentence) {
+    try {
+        if (!token) {
+            token = await authentication();
+            extensionContext.workspaceState.update(STORAGE_KEY, token);
+        }
+    } catch (err) { return; }
 
     // search in stackoverflow
-    const path = `https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=${language}&intitle=${sentence}&site=stackoverflow`;
+    const path = `https://api.stackexchange.com/2.3/search?order=desc&sort=activity&tagged=${language}&intitle=${sentence}&site=stackoverflow&access_token=${token}&key=${STACK_EXCHANGE_APP_KEY}`;
+    console.log(path)
     const doc = vscode.workspace.openTextDocument({
         language: language
     });
-    console.log(axios.get(path, { httpsAgent: agent }));
-    let resp = await axios.get(path, {
-        httpsAgent: agent
-    });
+
+    let resp = await axios.get(path, { httpsAgent: agent });
     let json = resp.data;
-    const posts = json["items"];
+    const posts = json.items;
     for (let i = 0; i < posts.length; i++) {
         if (posts[i]["is_answered"] == true && posts[i]["accepted_answer_id"] !== undefined) {
             let id = posts[i]["accepted_answer_id"];
-            const answer = `https://api.stackexchange.com/2.3/answers/${id}?order=desc&sort=activity&site=stackoverflow&filter=!nKzQURF6Y5`;
+            const answer = `https://api.stackexchange.com/2.3/answers/${id}?order=desc&sort=activity&site=stackoverflow&filter=!nKzQURF6Y5&access_token=${token}&key=${STACK_EXCHANGE_APP_KEY}`;
             let reponse = await axios.get(answer);
             let jsonresp = reponse.data;
-            let response = jsonresp["items"][0]["body"];
+            let response = jsonresp.items[0].body;
             const dom = new JSDOM(response);
             reponse = "/*\n" + dom.window.document.querySelector("code").textContent + "*/\r\n";
             responses.push(reponse);
